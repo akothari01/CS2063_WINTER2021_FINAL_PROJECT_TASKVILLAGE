733435b701b2948a28aff15220556814a36f86f1
isaiahDM <79055291+isaiahDM@users.noreply.github.com>
2021-03-01 11:38:31 -0400

    Add files via upload

--- sortTasks.java
+++ sortTasks.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Array;
 
 public class sortTasks{
 
-    public int compareTasks(Task task1, Task task2){
+    public int compareTasksByPriority(Task task1, Task task2){
 
         if(task1.getPriority() > task2.getPriority()){
             return 1;
@@ -23,7 +23,7 @@ public class sortTasks{
 
     }
 
-    public ArrayList<Task> sortTasks(ArrayList<Task> taskList){
+    public ArrayList<Task> sortTasksByPriority(ArrayList<Task> taskList){
 
         if(taskList.size() <= 1){
             return taskList;
@@ -35,7 +35,7 @@ public class sortTasks{
 
         Task pivot = taskList.get(taskList.size()-1);
         for(int i = 0; i < taskList.size()-1; i++){
-            if((compareTasks(taskList.get(i), pivot)) < 0){
+            if((compareTasksByPriority(taskList.get(i), pivot)) < 0){
                 lesser.add(taskList.get(i));
             }
 
@@ -45,8 +45,8 @@ public class sortTasks{
 
         }
 
-        lesser = sortTasks(lesser);
-        greater = sortTasks(greater);
+        lesser = sortTasksByPriority(lesser);
+        greater = sortTasksByPriority(greater);
 
         lesser.add(pivot);
         lesser.addAll(greater);
@@ -55,6 +55,70 @@ public class sortTasks{
         return sorted;
 
 
+
+    }
+
+    public int compareTasksByDeadline(Task task1, Task task2){
+
+        if(task1.getDeadline().after(task2.getDeadline())){
+            return 1;
+        }
+
+        else if(task1.getDeadline().before(task2.getDeadline())){
+            return -1;
+        }
+
+        else{
+            return 0;
+        }
+
+    }
+
+    public ArrayList<Task> sortTasksByDeadline(ArrayList<Task> taskList){
+
+        if(taskList.size() <= 1){
+            return taskList;
+        }
+
+        ArrayList<Task> sorted = new ArrayList<Task>();
+        ArrayList<Task> lesser = new ArrayList<Task>();
+        ArrayList<Task> greater = new ArrayList<Task>();
+
+        Task pivot = taskList.get(taskList.size()-1);
+        for(int i = 0; i < taskList.size()-1; i++){
+            if((compareTasksByDeadline(taskList.get(i), pivot)) < 0){
+                lesser.add(taskList.get(i));
+            }
+
+            else{
+                greater.add(taskList.get(i));
+            }
+
+        }
+
+        lesser = sortTasksByDeadline(lesser);
+        greater = sortTasksByDeadline(greater);
+
+        lesser.add(pivot);
+        lesser.addAll(greater);
+        sorted = lesser;
+
+        return sorted;
+
+
+    }
+
+    public ArrayList<Task> sortTasks(ArrayList<Task> taskList, boolean byDeadline){
+
+        if(byDeadline){
+            taskList = sortTasksByDeadline(taskList);
+        }
+
+        else{
+            taskList = sortTasksByPriority(taskList);
+        }
+
+        return taskList;
 
     }
 
